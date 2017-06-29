4cbcd7eccbe39277d11689d701c474258875fc65
Nathan Demesew <nathandemes@gmail.com>
2017-06-29 20:38:04 +0000

    Created dawg thing

---
+++ that_was_easy/main.js
@@ -0,0 +1,17 @@
+var thatWasEasy = new Audio("that _was_easy.mp3");
+thatWasEasy.play();
+
+function sayThatWasEasy() {
+  var thatWasEasy = new Audio("that_was_easy.mp3");
+  thatWasEasy.play();
+}
+
+$("#easy").on("click", sayThatWasEasy);
+
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event) {
+  if (event.charCode == 32) {
+    $("#easy").trigger("click");
+  }
+}
